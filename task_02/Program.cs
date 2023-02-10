74d66cf44d88ab51e8a3494b897d40ccf2d8a47a
Nurlan Ormond <sunshineforester@gmail.com>
2023-02-10 18:28:20 +0300

    Task_2 Max of three

---
+++ task_02/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine ("Hello, provide three number to know which is max!");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max =a;
+
+if (b>a) {
+    max = b;
+
+}
+
+if (c>b) {
+    max = c;
+}
+
+Console.WriteLine("The greatest number is:  " +max);
