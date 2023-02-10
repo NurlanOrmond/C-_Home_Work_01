a62993c7b04c160615da6e228e749cf72fd6f7a3
Nurlan Ormond <sunshineforester@gmail.com>
2023-02-10 19:15:09 +0300

    task_03 Even or Odd

---
+++ task_03/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.WriteLine("Give three numbers to know if they are even");
+Console.Write ("Number 1:   ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Number 2:   ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Number 3:   ");
+int c = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("****************");
+
+if ( a %2 == 0 ) {
+Console.Write(a);
+Console.WriteLine ("   - is Even!");
+} else {
+    Console.Write(a);
+    Console.WriteLine ("   - is Odd!");
+    }
+
+
+if ( b %2 == 0 ) {
+Console.Write(b);
+Console.WriteLine ("   - is Even!");
+} else {
+    Console.Write(b);
+    Console.WriteLine ("   - is Odd!");
+    }
+
+
+if ( c %2 == 0 ) {
+Console.Write(c);
+Console.WriteLine ("   - is Even!");
+} else {
+    Console.Write(c);
+    Console.WriteLine ("   - is Odd!");
+    }   
+Console.WriteLine("****************");
+
+ 
+ 
+
