143728866fd16aa80182b6ad6b605dbe45a77e86
Nurlan Ormond <sunshineforester@gmail.com>
2023-02-10 20:31:43 +0300

    task_04 All even number of range

---
+++ task_04/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine ("**********************************************************************************");
+Console.WriteLine ("Give me a number then I'll show you all even numbers in range from 0 to your number");
+Console.Write ("Your number:   ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+for (int i = 0; i <= n; i++) {
+        if (i%2==0) {
+                Console.Write (i);
+                Console.Write (" ");
+
+        }
+        
+}
+
