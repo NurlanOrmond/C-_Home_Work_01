822944a019ed790f382091f23696b22d8d34c33a
Nurlan Ormond <sunshineforester@gmail.com>
2023-02-10 18:14:43 +0300

    task_01 max of two numbers

---
+++ task_01/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Input two numbers:  ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a>b){
+    Console.WriteLine ("max number is:  " +a);
+} 
+else
+Console.WriteLine ("max number is:  " +b);
